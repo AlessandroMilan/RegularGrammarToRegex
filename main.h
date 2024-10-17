b9c3ec5b21d0ce272976d0a068e71f50d51f8f73
Alessandro Milan <amilan@data-keeper.com>
2024-10-17 10:26:29 -0600

    Adding sample grammar

--- main.h
+++ main.h
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define MAX_LINE_LENGTH 100
+
+// Node structure for the linked list
+typedef struct Node {
+    char *ruleIdentifier;
+    char *production;
+    struct Node *next;
+} Node;
+
+Node* createNode(const char *ruleIdentifier, const char *production);
+void appendNode(Node **head, const char *ruleIdentifier, const char *production);
+void freeLinkedList(Node *head);
+Node* createLinkedList(FILE *file);
+void printList(Node *head);
